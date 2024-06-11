33e25569e8eda27a33c1c89d15930fb65f4bd89e
MudassarSharifSandhu <mudassarshariffsandhu@gmail.com>
2024-06-12 03:52:14 +0500

    react selecter done

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FormulaInput from "./Components/FormulaInput";
+
+function App() {
+  return  <FormulaInput />;
+}
+
+export default App;
---
+++ src/Components/FormulaInput.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { autocompletion } from "@codemirror/autocomplete";
+
+const dummyVariables = {
+  "Actual New MRR": 1000,
+  "New Customers": 10,
+  "Monthly ACV": 100,
+};
+
+const FormulaInput = () => {
+  const [formula, setFormula] = useState("");
+
+  const handleChange = (value) => {
+    setFormula(value);
+  };
+
+  const handleAutocomplete = (context) => {
+    const word = context.matchBefore(/\w*/);
+    if (word.from === word.to && !context.explicit) return null;
+    return {
+      from: word.from,
+      options: [
+        { label: "Actual New MRR", type: "variable" },
+        { label: "New Customers", type: "variable" },
+        { label: "Monthly ACV", type: "variable" },
+      ],
+    };
+  };
+
+  const generateSafeVariableName = (name) => name.replace(/ /g, "_");
+
+  const evaluateFormula = (formula) => {
+    try {
+      const variableMapping = {};
+      let safeFormula = formula;
+
+      for (const [key, value] of Object.entries(dummyVariables)) {
+        const safeName = generateSafeVariableName(key);
+        safeFormula = safeFormula.replace(new RegExp(key, "g"), safeName);
+        variableMapping[safeName] = value;
+      }
+
+      const evaluated = new Function(
+        "variables",
+        `with(variables) { return ${safeFormula}; }`
+      )(variableMapping);
+      console.log("evaluated", evaluated);
+      return evaluated;
+    } catch (e) {
+      return "Invalid formula";
+    }
+  };
+
+  return (
+    <div>
+      <CodeMirror
+        value={formula}
+        height="200px"
+        extensions={[
+          javascript(),
+          autocompletion({ override: [handleAutocomplete] }),
+        ]}
+        onChange={handleChange}
+      />
+      <div>
+        <strong>Evaluated Result:</strong> {evaluateFormula(formula)}
+      </div>
+    </div>
+  );
+};
+
+export default FormulaInput;
